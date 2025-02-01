c0dfcf9a6c458f9c2bb52b6f0eb4d43b3865b81a
raudelf <rawdell@outlook.com>
2025-02-01 12:56:37 -0600

    restructuring app and creating components to build out the header

---
+++ src/components/StormHeader.vue
@@ -0,0 +1,52 @@
+<script setup>
+import StormMobileMenu from './StormMobileMenu.vue'
+
+const handleFormSubmit = (e) => {
+  e.preventDefault()
+  console.log(e.target)
+}
+</script>
+
+<template>
+  <div class="storm-header__body">
+    <div class="storm-container">
+      <div class="storm-header__row">
+        <div class="storm-header__logo">
+          <a href="/" class="storm-header__logo-link">
+            <div class="storm-header__img-container">
+              <img
+                alt="Storm"
+                class="storm-header__img"
+                src="../assets/images/storm-header-logo.png"
+              />
+            </div>
+          </a>
+        </div>
+
+        <StormMobileMenu />
+
+        <!-- <div class="storm-header__utils">
+          <ul class="storm-utils-nav">
+            <li class="storm-utils-nav__item storm-utils-nav__item--search"></li>
+          </ul>
+        </div> -->
+      </div>
+
+      <div class="storm-header__row storm-header__row--mobile">
+        <div class="storm-header__search-container">
+          <form @submit="handleFormSubmit" class="storm-header__search-form">
+            <label for="header-search" class="storm-visually-hidden">Search for a product</label>
+            <input
+              id="header-search"
+              type="text"
+              name="header-search"
+              class="storm-header__search-input"
+              placeholder="Search"
+            />
+            <button class="storm-btn storm-header__search-btn" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/StormMobileMenu.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+import HamburgerMenuSVG from './SVGs/HamburgerMenuSVG.vue'
+import HamburgerMenuCloseSVG from './SVGs/HamburgerMenuCloseSVG.vue'
+
+const isOpen = ref(false)
+
+const handleOpenMenu = () => {
+  isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+  <div class="storm-header__mobile-nav">
+    <nav class="storm-main-nav">
+      <button
+        type="button"
+        class="storm-main-nav__btn"
+        @click="handleOpenMenu"
+        v-bind:aria-expanded="isOpen"
+        aria-label="Toggle Navigation Menu"
+      >
+        <HamburgerMenuSVG v-show="!isOpen" />
+        <HamburgerMenuCloseSVG v-show="isOpen" />
+      </button>
+      <div
+        v-bind:class="
+          isOpen ? 'storm-main-nav__menu storm-main-nav__menu--expanded' : 'storm-main-nav__menu'
+        "
+      >
+        <div class="storm-container">
+          <ul class="storm-main-nav__list">
+            <li class="storm-main-nav__item">
+              <a href="/" class="storm-main-nav__link">Settings</a>
+            </li>
+            <li class="storm-main-nav__item">
+              <a href="/" class="storm-main-nav__link">My Account</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
