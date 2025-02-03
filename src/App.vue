8ab2e8de51375ac619b18cb55488c08243daae9c
raudelf <rawdell@outlook.com>
2025-02-02 22:42:40 -0600

    implementing functionality to call in the products data + passing in data into table component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import StormHeader from './components/StormHeader.vue'
+import ProductTable from './components/ProductTable.vue'
+
+const products = ref([])
+const fetchError = ref('')
+const isLoading = ref(true)
+
+onMounted(async () => {
+  try {
+    const response = await fetch('/products.json')
+
+    if (!response.ok) {
+      fetchError.value = 'Failed to fetch data.'
+      throw new Error('Failed to fetch data')
+    }
+
+    const data = await response.json()
+
+    products.value = data
+  } catch (error) {
+    fetchError.value = 'Error Fetching JSON'
+
+    console.error('Error Fetching JSON:', error)
+  } finally {
+    isLoading.value = false
+  }
+})
 </script>
 
 <template>
@@ -7,7 +34,17 @@ import StormHeader from './components/StormHeader.vue'
     <StormHeader />
   </header>
 
-  <main class="storm-main"></main>
-</template>
+  <main class="storm-main">
+    <div class="storm-container">
+      <div class="storm-main__title-container">
+        <h1 class="storm-main__title">Products</h1>
+        <span v-if="!isLoading && !fetchError"
+          >{{ products.length }} of {{ products.length }} results</span
+        >
+      </div>
 
-<style scoped lang="scss"></style>
+      <ProductTable v-if="!fetchError" :products="products" />
+      <p class="" v-else>{{ fetchError }}</p>
+    </div>
+  </main>
+</template>
